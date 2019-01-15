0f18b39bc9cc5dcc1d01f2166fcea5f8db6eea6b
Eyad Kelleh <33716629+Eyadkelleh@users.noreply.github.com>
2019-01-15 18:06:58 +0100

    I made all the information about the design patterns

---
+++ TrackerLibary/TrackerUI/.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TrackerUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TiurnamentName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Roundlbl = new System.Windows.Forms.Label();
+            this.RoundDropDown = new System.Windows.Forms.ComboBox();
+            this.UnplayedOnlyCheckbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // TiurnamentName
+            // 
+            this.TiurnamentName.AutoSize = true;
+            this.TiurnamentName.Font = new System.Drawing.Font("Segoe UI Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TiurnamentName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.TiurnamentName.Location = new System.Drawing.Point(12, 9);
+            this.TiurnamentName.Name = "TiurnamentName";
+            this.TiurnamentName.Size = new System.Drawing.Size(169, 40);
+            this.TiurnamentName.TabIndex = 0;
+            this.TiurnamentName.Text = "Tournament:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(187, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "<none>";
+            // 
+            // Roundlbl
+            // 
+            this.Roundlbl.AutoSize = true;
+            this.Roundlbl.Font = new System.Drawing.Font("Segoe UI Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Roundlbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.Roundlbl.Location = new System.Drawing.Point(12, 74);
+            this.Roundlbl.Name = "Roundlbl";
+            this.Roundlbl.Size = new System.Drawing.Size(97, 40);
+            this.Roundlbl.TabIndex = 2;
+            this.Roundlbl.Text = "Round";
+            // 
+            // RoundDropDown
+            // 
+            this.RoundDropDown.FormattingEnabled = true;
+            this.RoundDropDown.Location = new System.Drawing.Point(134, 76);
+            this.RoundDropDown.Name = "RoundDropDown";
+            this.RoundDropDown.Size = new System.Drawing.Size(171, 38);
+            this.RoundDropDown.TabIndex = 3;
+            // 
+            // UnplayedOnlyCheckbox
+            // 
+            this.UnplayedOnlyCheckbox.AutoSize = true;
+            this.UnplayedOnlyCheckbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UnplayedOnlyCheckbox.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UnplayedOnlyCheckbox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.UnplayedOnlyCheckbox.Location = new System.Drawing.Point(134, 145);
+            this.UnplayedOnlyCheckbox.Name = "UnplayedOnlyCheckbox";
+            this.UnplayedOnlyCheckbox.Size = new System.Drawing.Size(146, 29);
+            this.UnplayedOnlyCheckbox.TabIndex = 4;
+            this.UnplayedOnlyCheckbox.Text = "Unplayed Only";
+            this.UnplayedOnlyCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(499, 418);
+            this.Controls.Add(this.UnplayedOnlyCheckbox);
+            this.Controls.Add(this.RoundDropDown);
+            this.Controls.Add(this.Roundlbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TiurnamentName);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Tournament Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TiurnamentName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Roundlbl;
+        private System.Windows.Forms.ComboBox RoundDropDown;
+        private System.Windows.Forms.CheckBox UnplayedOnlyCheckbox;
+    }
+}
+
